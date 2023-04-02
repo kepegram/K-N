de9cd68288b5cd9300b5cdb75a2439ed93cdb7ed
Kadin Pegram <kpegra1@students.towson.edu>
2023-04-02 00:21:31 -0400

    Upated mainpage to start!

---
+++ src/Mainpage.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Mainpage() {
+  return (
+    <div className="Mainpage">
+      <header className="Mainpage-header">
+        <p>
+          Welcome to our website!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Mainpage;
